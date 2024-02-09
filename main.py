1c164435d82b7af39dc6e50628c86be7642a8a5f
Victor123321123321 <KopytovVictor@mail.ru>
2024-02-09 14:36:25 +0300

    сломанный код 2

--- main.py
+++ main.py
@@ -4,16 +4,4 @@ class A:
 
 spisok = []
 
-max_ball = 0
-N = int(input())
-for i in range(N):
-    spisok.append(A())
-    spisok[i].firma, spisok[i].ulica, spisok[i].jirnost, spisok[i].cent = map(str, input().split())
-
-count1, count2, count3 = 0, 0, 0
-max1, max2, max3 = 0, 0, 0
-for j in range(N):
-    if
-
-print(N / counnt)
-
+=

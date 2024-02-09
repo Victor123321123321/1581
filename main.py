78713d94fd1e4bec24455029e569d26350402901
Victor123321123321 <KopytovVictor@mail.ru>
2024-02-09 15:28:48 +0300

    Готовый код

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-class A:
-    pass
+class Student():
+    def __init__(self, inf):
+        self.id = inf[0]
+        self.Name = inf[1].split()
+        self.title = inf[2]
+        self.clas = inf[3]
+        self.score = inf[4]
 
 
+f = open("students.csv")
+f.readline()
 spisok = []
+for i in f:
+    inf = i.split(',')
+    s = Student(inf)
+    spisok.append(s)
 
-=
+for i in range(1, len(spisok)):
+    t = spisok[i].id
+    j = i - 1
+    while j >=0 and t < spisok[j].id:
+        spisok[j + 1].id = spisok[j].id
+        j -= 1
+    spisok[j + 1].id = t
+
+n = 1
+for i in range(len(spisok)):
+    if spisok[i].clas[:2] == "10":
+        print(str(n) + " место: " + spisok[i].Name[1][0] + '.', spisok[i].Name[0])
+        n += 1
+    if n == 4:
+        break
